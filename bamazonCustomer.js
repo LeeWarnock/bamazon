bdb0c755f2e173a3bc1569f847b52e363012053b
Lee Warnock <clwarnock@gmail.com>
2016-07-16 12:34:39 -0500

    adds theGoods product databse

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,8 +5,8 @@ var connection=mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
-    database: 'bamazon_db'
+    password: 'Kelcy',
+    database: 'theGoods'
 })
 //checks connection
 connection.connect(function(err){
